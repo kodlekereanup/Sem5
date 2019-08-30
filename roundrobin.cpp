87f597742ded85bf42cbb74c518be300848bd6b5
trail99 <anup.kodlekere@gmail.com>
2019-08-30 13:26:35 +0530

    change gantt

--- roundrobin.cpp
+++ roundrobin.cpp
@@ -5,6 +5,11 @@
 #include <iterator>
 #include <queue>
 
+int currentTime = 0;
+std::queue<int> scheduleQueue;
+std::vector<int> orderOfExec;
+std::vector<float> intermediateFinishTime;
+
 enum status { NOT_ARRIVED, IN_QUEUE, EXECUTING, SUSPENDED, SCHEDULED };
 
 struct Process {
@@ -17,34 +22,31 @@ struct Process {
 	float waitingTime;
 	status state;
 	
-	float scheduleProcess(int);
+	float scheduleProcess(int, int, int);
 
 };
 
-void sort(std::vector<Process>& proc) {
-	int n = proc.size();
-	for (int i = 0; i < n - 1; i++)
-		for (int j = 0; j < n - i - 1; j++)
-			if (proc[j].finishTime > proc[j + 1].finishTime)
-				std::swap(proc[j], proc[j + 1]);
-
-}
-
-float Process :: scheduleProcess(int prevFinishTime) {
+float Process :: scheduleProcess(int prevFinishTime, int timer, int i) {
 	state = EXECUTING;
-	finishTime = burstTime + prevFinishTime;
+	finishTime = timer + prevFinishTime;
+    intermediateFinishTime.push_back(finishTime);
+    burstTime -= timer;
 	turnAroundTime = finishTime - arrivalTime;
-	waitingTime = turnAroundTime - burstTime;
-	state = SCHEDULED;	
-	return finishTime;
+
+    orderOfExec.push_back(i);
+	
+    if(burstTime == 0) state = SCHEDULED;
+    else state = IN_QUEUE;
+	
+	return finishTime; // or timer
 }
 
 // check if time occurs in p vector and return pid //
-std::vector<int> arrivedProccesses(int t, std::vector<Process>& P) {
+std::vector<int> arrivedProccesses(std::vector<Process>& P) {
 	bool flag = false; int i = 0;   std::vector<int> queue;
 	for (i = 0; i < P.size(); i++) {
 		flag = false;
-		if (P[i].arrivalTime <= t && (P[i].state == NOT_ARRIVED || P[i].state == IN_QUEUE)) {
+		if (P[i].arrivalTime <= currentTime && P[i].state == NOT_ARRIVED) {
 			flag = true;
 			P[i].state = IN_QUEUE;
 			queue.push_back(i);
@@ -52,41 +54,6 @@ std::vector<int> arrivedProccesses(int t, std::vector<Process>& P) {
 	}
 
 	return queue;
-}
-
-int maxArrivalTime(const std::vector<Process>& P) {
-	int max = P[0].arrivalTime;
-	for (int i = 0; i < P.size(); i++) if (P[i].arrivalTime > max) max = P[i].arrivalTime;
-	return max;
-}
-
-int findInProcess(const std::vector<Process>& proc, int k){  
-   for (int i = 0; i < proc.size(); i++){  
-      if (proc[i].burstTime == k)
-         return i;
-   }
-   return -1;
-}
-
-// find the occurrences of all shortestburtesttime 
-std::vector<int> findNextShortest(const std::vector<Process>& proc, std::vector<int> arrivedProc) {
-	std::vector<int> temp, temp2;	
-	float btShort = proc[arrivedProc.at(0)].burstTime;
-	
-    for(int i = 0; i < arrivedProc.size(); i++) {
-        temp.push_back(proc[arrivedProc.at(i)].burstTime);
-    }
-
-    // find the shortest value in temp and return its index  
-    int min = temp.at(0); int index = 0;
-    for(int i = 0; i < temp.size(); i++) {
-        if(temp[i] < min) min = temp.at(i);         
-    }
-    
-    index = findInProcess(proc, min);    
-    temp2.push_back(index);
-
-	return temp2;
 }
 
 void displayTable(const std::vector<Process>& P) {
@@ -109,10 +76,10 @@ void displayGanttChart(const std::vector<Process>& proc) {
 	std::cout << "\n|";
 
 	//print process
-	for (int i = 0; i < proc.size(); i++) {
-		for (int j = 0; j < proc[i].burstTime - 1; j++) std::cout << " ";
-		std::cout << "P" << proc[i].id;
-		for (int j = 0; j < proc[i].burstTime - 1; j++) std::cout << " ";
+	for (int i = 0; i < orderOfExec.size(); i++) {
+		for (int j = 0; j < intermediateFinishTime.at(i) - 1; j++) std::cout << " ";
+		std::cout << "P" << orderOfExec.at(i);
+		for (int j = 0; j < intermediateFinishTime.at(i) - 1; j++) std::cout << " ";
 		std::cout << "|";
 	}
 
@@ -127,10 +94,10 @@ void displayGanttChart(const std::vector<Process>& proc) {
 
 	// printing the finish time
 	std::cout << "0";
-	for (int i = 0; i < proc.size(); i++) {
-		for (int j = 0; j < proc[i].burstTime; j++) std::cout << "  ";
-		if (proc[i].finishTime > 9) std::cout << "\b";
-		std::cout << proc[i].finishTime;
+	for (int i = 0; i < intermediateFinishTime.size(); i++) {
+		for (int j = 0; j < intermediateFinishTime.at(i); j++) std::cout << "  ";
+		if (intermediateFinishTime.at(i) > 9) std::cout << "\b";
+		std::cout << intermediateFinishTime.at(i);
 
 	}
 	std::cout << "\n\n";
@@ -149,11 +116,12 @@ void displayStats(const std::vector<Process>& proc) {
 	std::cout << "\n Average Turn Around Time: " << tsum / proc.size() << "ms";
 	std::cout << "\n Average Waiting Time:     " << wsum / proc.size() << "ms";
 
+    std::cout << "\n";
+
 }
 
 bool complete(const std::vector<Process>& proc) {
     bool check = true;
-
     for(int i = 0; i < proc.size(); i++) {
         if(proc[i].state != SCHEDULED) {
             check = false;
@@ -166,10 +134,8 @@ bool complete(const std::vector<Process>& proc) {
 
 int main() {
 
-    bool completed = true;
-	int n, id, currentTime = 0;
-	float at, bt, ft, wt, tt, prevFinishTime;
-    int timeQuantum;
+    int n, id, s;
+	float at, bt, ft, wt, tt, prevFinishTime, timeQuantum;
 
 	std::cout << "\n Enter the number of processes:";
 	std::cin >> n;
@@ -194,26 +160,61 @@ int main() {
 	std::cout << "\n Enter the Time Quantum: ";
     std::cin >> timeQuantum;
 
-    currentTime = 0;
-    while(!complete(process)) {
-        std::vector<int> pid = arrivedProccesses(currentTime, process);
-        std::vector<int> schedulable = findNextShortest(process, pid);
-
-        for(int i = 0; i < schedulable.size(); i++) {            
-            if(i == 0 && currentTime == 0) {               
-                prevFinishTime = process[schedulable.at(i)].scheduleProcess(0);
-                currentTime = prevFinishTime;
-            } else {               
-                prevFinishTime = process[schedulable.at(i)].scheduleProcess(prevFinishTime);               
-                currentTime = prevFinishTime;
-            }
+    std::vector<Process> copy = process;
+    
+    bool flag = false;
+    currentTime = 0, prevFinishTime = 0;    
+    while(!complete(process)){
+        
+        std::vector<int> pid = arrivedProccesses(process);      
+        for(int i = 0; i < pid.size(); i++) scheduleQueue.push(pid.at(i));
+        
+        if(flag) scheduleQueue.push(s);       
+       
+        if(process.at(scheduleQueue.front()).burstTime < timeQuantum) 
+            prevFinishTime = process.at(scheduleQueue.front()).scheduleProcess(prevFinishTime, process.at(scheduleQueue.front()).burstTime, process.at(scheduleQueue.front()).id);                                  
+        else prevFinishTime = process.at(scheduleQueue.front()).scheduleProcess(prevFinishTime, timeQuantum, process.at(scheduleQueue.front()).id);                                        
+                            
+        if(process.at(scheduleQueue.front()).burstTime != 0) { 
+            flag = true;               
+            s = scheduleQueue.front();
+            scheduleQueue.pop();
+        } else {
+            flag = false;
+            scheduleQueue.pop();
         }
+        currentTime = prevFinishTime;
+                 
     }
-	
+
+    for(int i = 0; i < process.size(); i++) {
+        copy.at(i).finishTime = process.at(i).finishTime;
+        copy.at(i).turnAroundTime = process.at(i).turnAroundTime;
+        copy.at(i).waitingTime = copy.at(i).turnAroundTime - copy.at(i).burstTime;
+    }
+
+    process = copy;
 	displayTable(process);
-	sort(process);
+    
 	displayGanttChart(process);
 	displayStats(process);
 
 	return 0;
 }
+
+// TODO:
+// Draw Gantt chart 
+/*
+Round Robin Algorithm:
+
+1. Find the process that arrives first
+2. If it's burst time is less than the time quantum, schedule using 
+   it's burst time. Else use time quantum.
+3. If scheduled using quantum, update process bursttime = bursttime - timequantum
+    NOTE: Make sure it doesn't go negative 
+4. Check for the processes that have arrived (FCFS)
+5. Schedule the next one in the same way
+6. Stop when all process burst times = 0
+
+
+*/
